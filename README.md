e3c0247ade5cf010ac7b8f8d31c14f095a8d688b
CrabcakeODA <46054396+CrabcakeODA@users.noreply.github.com>
2020-11-07 02:25:22 -0500

    re structure

--- README.md
+++
@@ -1,8 +0,0 @@
-# expo starter template
-
-Features
-1. react navigation 
-2. redux
-3. i18
-4. loading screens
-5. global params and styles
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,5 @@
 import * as Buttons from './buttons'
-import * as Colors from './colors'
+import * as Colors from '../configs/StyleConfig'
 import * as Spacing from './spacing'
 import * as Typography from './typography'
 
